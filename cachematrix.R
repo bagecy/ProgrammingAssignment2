93d0c7bfe86c124503dc32f706703ba8a3f7b265
yi chen <yichen.aggie@hotmail.com>
2017-04-13 01:15:19 -0500

    Update cachematrix.R
    adding the code for the project in week3

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y   #y is an input value of set, this makes input x has 2 environment, 2 values
+    m <<- NULL
+  }
+  get <- function() x # x has become the value in cache environment, after last function, this return the cached value
+  setinv <- function(inv) m <<- inv #like set, mean is cache, m is NULL here, but might have value in cache
+  getinv <- function() m # get the value of m in cache environment
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()              # get the value of m in cache environment
+  if(!is.null(m)) {              #check if mean in the cache has value, if yes, return the value
+    message("getting cached data") # if no, go to next command
+    return(m)
+  }
+  data <- x$get()             # give the cached value to data
+  m <- solve(data)        # calculate mean of the cached value and give it to m
+  x$setinv(m)                # saved the value of m to the cache environment
+  m   ## Return a matrix that is the inverse of 'x'
 }
